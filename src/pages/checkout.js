37244a45a156f1da1912655fc3e7e057739de859
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-15 13:44:46 +0300

    Initial Committ

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -9,7 +9,7 @@ import Currency from "react-currency-formatter";
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
-  // my name
+  // my name is abel
   const [session] = useSession();
   return (
     <div className="bg-gray-100">
